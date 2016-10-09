ecbd3b52642de09e7f15cf893ed8601eb04d80cd
Charlie <chenlei.hu@mail.utoronto.ca>
2016-10-08 22:04:14 -0400

    excutable included

--- lab/lab3/main.cpp
+++ lab/lab3/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include "Rparser.h"
+//
+//int main() {
+//    string str;
+//
+//    cout << "> ";
+//    getline(cin, str);
+//
+//    Rparser rp;
+//    while (!cin.eof()) {
+//        cout << rp.parse(str) << endl;
+//        cout << "> ";
+//        getline(cin, str);
+//    }
+//    return 0;
+//}
+
 
 int main() {
     string str;
-
+    
     cout << "> ";
     getline(cin, str);
 
